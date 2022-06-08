1b6fe033c0cbb32a300548f3c6a29e5ec9950caa
MasatoMakino <makino.masato.g@gmail.com>
2022-06-08 16:44:37 +0900

    ignore : bin

--- esm/index.js
+++
@@ -1,6 +0,0 @@
-export * from "./SliderEvent";
-export * from "./SliderView";
-export * from "./scrollBar/ScrollBarView";
-export * from "./SliderViewOption";
-import { initPlugin } from "./MouseWheelPlugin";
-initPlugin();
--- lib/scrollBar/ScrollBarEvent.d.ts
+++
@@ -1,5 +0,0 @@
-export declare enum ScrollBarEventType {
-    UPDATE_TARGET_POSITION = "ScrollBarEventType_UPDATE_TARGET_POSITION",
-    STOP_INERTIAL_TWEEN = "ScrollBarEventType_STOP_INERTIAL_TWEEN"
-}
-//# sourceMappingURL=ScrollBarEvent.d.ts.map
