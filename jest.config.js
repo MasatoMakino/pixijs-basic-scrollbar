4e7d598c7b175dced09b1627af42d9534506f52f
MasatoMakino <makino.masato.g@gmail.com>
2023-03-10 16:57:04 +0900

    migrate : @swc/jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 const { defaults } = require("jest-config");
 
 module.exports = {
-  preset: "ts-jest",
   testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  },
   coveragePathIgnorePatterns: [
     ...defaults.coveragePathIgnorePatterns,
     "__test__",
