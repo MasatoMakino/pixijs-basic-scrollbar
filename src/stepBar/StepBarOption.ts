7266a7c14e82e9a8aba105f96a9a970cd6eddcbf
MasatoMakino <makino.masato.g@gmail.com>
2025-02-28 11:57:06 +0900

    feat: add detailed documentation for StepBarOption interface parameters

--- src/stepBar/StepBarOption.ts
+++ src/stepBar/StepBarOption.ts
@@ -2,6 +2,26 @@ import { Container } from "pixi.js";
 
 /**
  * ステップバーのオプションです。
+ *
+ * ステップバーは、Pixi.jsの座標系に合わせてスクリーン座標系を使用します。
+ * 従って値が加算されると、スライダーの位置は右か下に移動します。
+ *
+ * 移動方向を逆転させたい場合は、以下のように設定してください。
+ * - sliderStartPointとsliderMaxPointを逆に指定する
+ * - incrementButtonとdecrementButtonを入れ替えて指定する。
+ *
+ * @param base ドラッグ操作を受け付ける範囲
+ * @param sliderStartPoint minValueに対応するスライダーの座標
+ * @param sliderMaxPoint maxValueに対応するスライダーの座標
+ * @param maxValue 最大値
+ * @param minValue 最小値
+ * @param initialValue 初期値
+ * @param step ステップ
+ * @param sliderButton スライダーボタン
+ * @param incrementButton ステップアップボタン
+ * @param decrementButton ステップダウンボタン
+ * @param isHorizontal 水平方向かどうか
+ * @param canvas キャンバス
  */
 export interface StepBarOption {
   base: Container; // 操作を受け付ける範囲
